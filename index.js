233b37da843346ca208e5868c711acbadf3ba979
Ahsan Ihsan <33997676+ahsanihsan@users.noreply.github.com>
2021-08-09 02:12:35 +0500

    Added rouiter testing and socket code for connect/disconnect

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ app.listen();
 
 const server = http.createServer(app);
 const io = socketio(server);
+const router = require("./router");
 
 const PORT = process.env.PORT || 8080;
+
+app.use(router);
+
+io.on("connection", (socket) => {
+  console.log("User just connected");
+  socket.on("disconnect", () => {
+    console.log("User just disconnected");
+  });
+});
 
 server.listen(PORT, () => {
   console.log("Listening to port " + PORT);

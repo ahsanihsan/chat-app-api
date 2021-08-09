514fcbabbce2349e419b93e438551c21bd24658e
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2021-08-09 19:33:02 +0500

    Package.json finalised and models added for users to save the signed up users

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const socketio = require("socket.io");
+const cors = require("cors");
 const http = require("http");
+const PORT = process.env.PORT || 8080;
 
 const app = express();
-
-app.listen();
-
 const server = http.createServer(app);
-const io = socketio(server);
-const router = require("./router");
+const io = socketio(server, {
+  cors: true,
+  origins: ["http://localhost:3000"],
+});
 
-const PORT = process.env.PORT || 8080;
+const router = require("./router");
 
 app.use(router);
 
 io.on("connection", (socket) => {
   console.log("User just connected");
-  socket.on("disconnect", () => {
-    console.log("User just disconnected");
+
+  socket.on("disconnect", ({ name, room }) => {
+    console.log(`A User just disconnected from room`);
   });
+
+  socket.on("join", ({ name, room }, callback) => {});
 });
 
 server.listen(PORT, () => {
---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
